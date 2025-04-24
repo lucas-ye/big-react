b17087f70310c86615c425b96f86416d1ea196f0
yeweijiang <yeweijiang@kuaishou.com>
2025-04-24 14:42:05 -0300

    chore: 开发环境配置

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,5 +1,5 @@
-import { REACT_ELEMENT_TYPE } from '../shared/ReactSymbols';
-import { Type, Key, Ref, Props, ReactElementType } from '../shared/ReactTypes';
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import { Type, Key, Ref, Props, ReactElementType } from 'shared/ReactTypes';
 // ReactElement
 
 const ReactElement = function (
